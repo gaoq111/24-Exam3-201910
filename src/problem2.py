e014000b8564dc429228acf701cf02827488d7aa
gaoq <gaoq@rose-hulman.edu>
2018-10-25 22:20:50 -0400

    exam 3

--- src/problem2.py
+++ src/problem2.py
@@ -2,13 +2,13 @@
 Exam 3, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  October 2018.
+         their colleagues and BERT.  October 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
-
+import math as mt
 
 def main():
     """ Calls the   TEST   functions in this module. """
@@ -90,10 +90,16 @@ def problem2(sequence):
       :type sequence [list]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
-
+    b = 0
+    a = mt.fabs(sequence[0])
+    for k in range(len(sequence)):
+        if a <= mt.fabs(sequence[k]):
+            a = mt.fabs(sequence[k])
+            b = k
+    return b
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
--- src/problem4.py
+++ src/problem4.py
@@ -2,8 +2,8 @@
 Exam 3, problem 4.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, their colleagues,
-         and PUT_YOUR_NAME_HERE.  October, 2018.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and BERT.  October, 2018.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 
 def main():
@@ -77,13 +77,32 @@ And this one for n=14:
     :type n: int
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Some tests are already written for you (above).
     ####################################################################
     # IMPORTANT: In your final solution for this problem,
     #   you must NOT use string multiplication.
     ####################################################################
-
+    for k in range(n):
+        for r in range(n-k-1):
+            print(' ',end='')
+        for j in range(k+1):
+            if j+1 >=10:
+                j = (j+1)%10
+                print(j,end='')
+            else:
+                print(j+1,end='')
+        print(' ',end = '')
+        for i in range(k+2):
+            print('*',end = '')
+        print(' ',end = '')
+        for t in range(n-k,0,-1):
+            if t>=10:
+                t = (t)%10
+                print(t,end='')
+            else:
+                print(t,end = '')
+        print()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
